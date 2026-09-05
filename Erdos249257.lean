92f012b5faf1732b3ed5f9f696dd2ebc1349a5e1
Will Cook <46428721+wcook04@users.noreply.github.com>
2026-09-05 10:53:16 +0100

    Integrate all-base reciprocal-summable support irrationality into main corpus

--- Erdos249257.lean
+++ Erdos249257.lean
@@ -4,6 +4,7 @@ import Erdos249257.ActualForeignResidueProjection
 import Erdos249257.AdelicHeightObstruction
 import Erdos249257.AdjacentCarryTube
 import Erdos249257.AdjacentPhaseSeparation
+import Erdos249257.AllBaseReciprocalSupportIrrationality
 import Erdos249257.BooleanMobiusCarry
 import Erdos249257.BooleanMobiusCriticalCapacityCofinal
 import Erdos249257.BooleanMobiusCriticalCapacityGeometric
